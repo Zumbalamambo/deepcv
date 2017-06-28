eedc360c93fb56bad4b82fe32a1ad105351c69ec
xhzcyc <sjsanlang@qq.com>
2017-06-28 17:13:28 +0800

    version 0.1

--- app/classifier.py
+++ app/classifier.py
@@ -25,6 +25,8 @@ def run(config, args):
             print('Classification error!')
     elif args.task == 'train':
         pass
+    elif args.task == 'test':
+        test()
     else:
         print('No this task!')
 
@@ -39,48 +41,52 @@ def classify_image_local(config, args):
     model_variables = config.get('weights', 'model_variables')
     image_path = args.file
 
-    # load file
-    image_ph = tf.placeholder(tf.float32, [None, img_height, img_width, 3])
-    image_original = Image.open(image_path)
-    image = image_original.resize((img_height, img_width))
-    image = np.expand_dims(np.array(np.uint8(image)), 0)
+    with tf.Graph().as_default():
+        # load file
+        image_ph = tf.placeholder(tf.float32, [None, img_height, img_width, 3])
+        image_original = Image.open(image_path)
+        image = image_original.resize((img_height, img_width))
+        image = np.expand_dims(np.array(np.uint8(image)), 0)
 
-    feed_dict = {image_ph: image}
+        feed_dict = {image_ph: image}
 
-    net_fn = classfier_factory.get_network_fn(model_name, num_class, is_training=False)
-    logits, _ = net_fn(image_ph)
-    net_out = tf.nn.softmax(logits)
+        net_fn = classfier_factory.get_network_fn(model_name, num_class, is_training=False)
+        logits, _ = net_fn(image_ph)
+        net_out = tf.nn.softmax(logits)
 
-    if args.gpu:
-        sess_cfg = tf.ConfigProto()
-        sess_cfg.gpu_options.allow_growth = True
-
-    with tf.Session(config=sess_cfg) as sess:
-        if os.path.isdir(ckpt_path):
-            saver = tf.train.Saver()
-            ckpt = tf.train.get_checkpoint_state(ckpt_path)
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
+        if args.gpu:
+            sess_cfg = tf.ConfigProto()
+            sess_cfg.gpu_options.allow_growth = True
+            sess = tf.Session(config=sess_cfg)
         else:
-            init_fn = slim.assign_from_checkpoint_fn(ckpt_path, slim.get_model_variables(model_variables))
-            init_fn(sess)
-        classify_result = sess.run(net_out, feed_dict=feed_dict)
-        probablities = classify_result[0, 0:]
-        sorted_id = [i[0] for i in sorted(enumerate(-probablities), key=lambda x: x[1])]
+            sess = tf.Session()
 
-    names = imagenet.create_readable_names_for_imagenet_labels()
-    top5_result = []
-    for i in range(5):
-        index = sorted_id[i]
-        top5_result.append({'name': names[index + 1], 'probability': probablities[index] * 100})
-        if args.print:
-            print('Probablity %.2f%% ==> [%s]' % ((probablities[index] * 100), names[index + 1]))
+        with sess:
+            if os.path.isdir(ckpt_path):
+                saver = tf.train.Saver()
+                ckpt = tf.train.get_checkpoint_state(ckpt_path)
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess, ckpt.model_checkpoint_path)
+            else:
+                init_fn = slim.assign_from_checkpoint_fn(ckpt_path, slim.get_model_variables(model_variables))
+                init_fn(sess)
+            classify_result = sess.run(net_out, feed_dict=feed_dict)
+            probablities = classify_result[0, 0:]
+            sorted_id = [i[0] for i in sorted(enumerate(-probablities), key=lambda x: x[1])]
 
-    if args.json:
-        top5_result = json.dumps(top5_result)
+        names = imagenet.create_readable_names_for_imagenet_labels()
+        top5_result = []
+        for i in range(5):
+            index = sorted_id[i]
+            top5_result.append({'name': names[index + 1], 'probability': probablities[index] * 100})
+            if args.print:
+                print('Probablity %.2f%% ==> [%s]' % ((probablities[index] * 100), names[index + 1]))
 
-    if args.show:
-        pass
+        if args.json:
+            top5_result = json.dumps(top5_result)
+
+        if args.show:
+            pass
     return top5_result
 
 
@@ -95,49 +101,95 @@ def classify_iamge_remote(config, args):
     model_variables = config.get('weights', 'model_variables')
     image_url = args.file_url
 
-    image_str = urllib.urlopen(image_url).read()
-    image = tf.image.decode_jpeg(image_str, channels=3)
+    with tf.Graph().as_default():
+        image_str = urllib.urlopen(image_url).read()
+        image = tf.image.decode_jpeg(image_str, channels=3)
 
-    preprocess_image_fn = preprocessing_factory.get_preprocessing(model_name, is_training=True)
+        preprocess_image_fn = preprocessing_factory.get_preprocessing(model_name, is_training=True)
 
-    processed_image = preprocess_image_fn(image, img_height, img_width)
-    processed_image = tf.expand_dims(processed_image, 0)
+        processed_image = preprocess_image_fn(image, img_height, img_width)
+        processed_image = tf.expand_dims(processed_image, 0)
 
-    net_fn = classfier_factory.get_network_fn(model_name, num_class, is_training=False)
-    logits, _ = net_fn(processed_image)
-    net_out = tf.nn.softmax(logits)
+        net_fn = classfier_factory.get_network_fn(model_name, num_class, is_training=False)
+        logits, _ = net_fn(processed_image)
+        net_out = tf.nn.softmax(logits)
 
-    if args.gpu:
-        sess_cfg = tf.ConfigProto()
-        sess_cfg.gpu_options.allow_growth = True
-
-    with tf.Session(config=sess_cfg) as sess:
-        if os.path.isdir(ckpt_path):
-            saver = tf.train.Saver()
-            ckpt = tf.train.get_checkpoint_state(ckpt_path)
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
+        if args.gpu:
+            sess_cfg = tf.ConfigProto()
+            sess_cfg.gpu_options.allow_growth = True
+            sess = tf.Session(config=sess_cfg)
         else:
-            init_fn = slim.assign_from_checkpoint_fn(ckpt_path, slim.get_model_variables(model_variables))
-            init_fn(sess)
+            sess = tf.Session()
 
-        np_image, probablities = sess.run([image, net_out])
-        probablities = probablities[0, 0:]
-        sorted_id = [i[0] for i in sorted(enumerate(-probablities), key=lambda x: x[1])]
+        with sess:
+            if os.path.isdir(ckpt_path):
+                saver = tf.train.Saver()
+                ckpt = tf.train.get_checkpoint_state(ckpt_path)
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess, ckpt.model_checkpoint_path)
+            else:
+                init_fn = slim.assign_from_checkpoint_fn(ckpt_path, slim.get_model_variables(model_variables))
+                init_fn(sess)
 
-    top5_result = []
-    names = imagenet.create_readable_names_for_imagenet_labels()
-    for i in range(5):
-        index = sorted_id[i]
-        top5_result.append({'name': names[index + 1], 'probability': probablities[index] * 100})
-        if args.print:
-            print('Probablity %.2f%% ==> [%s]' % ((probablities[index] * 100), names[index + 1]))
+            np_image, probablities = sess.run([image, net_out])
+            probablities = probablities[0, 0:]
+            sorted_id = [i[0] for i in sorted(enumerate(-probablities), key=lambda x: x[1])]
 
-    if args.json:
-        top5_result = json.dumps(top5_result)
+        top5_result = []
+        names = imagenet.create_readable_names_for_imagenet_labels()
+        for i in range(5):
+            index = sorted_id[i]
+            top5_result.append({'name': names[index + 1], 'probability': probablities[index] * 100})
+            if args.print:
+                print('Probablity %.2f%% ==> [%s]' % ((probablities[index] * 100), names[index + 1]))
 
-    if args.show:
-        pass
+        if args.json:
+            top5_result = json.dumps(top5_result)
+
+        if args.show:
+            pass
 
     return top5_result
 
+
+def test():
+    import model.classification.vgg as vgg
+    import utils.preprocessing.vgg_preprocessing as vgg_preprocessing
+    image_size = vgg.vgg_16.default_image_size
+    with tf.device('/gpu:0'):
+        url = 'https://upload.wikimedia.org/wikipedia/commons/d/d9/First_Student_IC_school_bus_202076.jpg'
+        image_string = urllib.urlopen(url).read()
+        image = tf.image.decode_jpeg(image_string, channels=3)
+        preprocess_image_fn = preprocessing_factory.get_preprocessing('vgg_16', is_training=True)
+
+        processed_image = preprocess_image_fn(image, image_size, image_size)
+
+        processed_image = vgg_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
+        processed_images = tf.expand_dims(processed_image, 0)
+
+        # Create the model, use the default arg scope to configure the batch norm parameters.
+        with slim.arg_scope(vgg.vgg_arg_scope()):
+            # 1000 classes instead of 1001.
+            logits, _ = vgg.vgg_16(processed_images, num_classes=1000, is_training=False)
+        probabilities = tf.nn.softmax(logits)
+
+        init_fn = slim.assign_from_checkpoint_fn(
+            os.path.join('cache/weight/vgg', 'vgg_16.ckpt'),
+            slim.get_model_variables('vgg_16'))
+
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+            init_fn(sess)
+            np_image, probabilities = sess.run([image, probabilities])
+            probabilities = probabilities[0, 0:]
+            sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x: x[1])]
+
+        # plt.figure()
+        # plt.imshow(np_image.astype(np.uint8))
+        # plt.axis('off')
+        # plt.show()
+
+        names = imagenet.create_readable_names_for_imagenet_labels()
+        for i in range(5):
+            index = sorted_inds[i]
+            # Shift the index of a class name by one.
+            print('Probability %0.2f%% => [%s]' % (probabilities[index] * 100, names[index + 1]))
--- app/detector.py
+++ app/detector.py
@@ -5,15 +5,6 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
-from google.protobuf import text_format
-
-import model.detection.trainer as trainer
-import model.detection.builders.model_builder as model_builder
-import model.detection.builders.input_reader_builder as input_reader_builder
-import model.detection.protos.model_pb2 as model_pb2
-import model.detection.protos.pipeline_pb2 as pipeline_pb2
-import model.detection.protos.train_pb2 as train_pb2
-import model.detection.protos.input_reader_pb2 as input_reader_pb2
 import utils.detection.label_map_util as label_map_util
 import utils.detection.visualization_utils as vis_util
 
@@ -29,16 +20,16 @@ def run(config, args):
         else:
             print('No this file!')
     elif args.task == 'train':
-        train_distributed(config, args)
+        pass
     else:
         print('No this task!')
 
 
 def detect_image_local(config, args):
     # parse arguments
-    num_class = int(config.get('dataset', 'num_class'))
-    label_path = config.get('dataset', 'label')
-    ckpt_path = config.get('weights', 'pb')
+    num_class = int(config.get('weight', 'num_class'))
+    label_path = config.get('weight', 'label')
+    ckpt_path = config.get('weight', 'pb')
     image_path = args.file
 
     # load file
@@ -62,8 +53,8 @@ def detect_image_local(config, args):
     category_index = label_map_util.create_category_index(categories)
     IMAGE_SIZE = (12, 8)
     with detection_graph.as_default():
-        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True),
-                        graph=detection_graph) as sess:
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True), graph=detection_graph) as sess:
+
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
             boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
             scores = detection_graph.get_tensor_by_name('detection_scores:0')
@@ -89,105 +80,3 @@ def detect_image_remote(config, args):
     pass
 
 
-def train_distributed(config, args):
-    train_dir = config.get('train', 'directory')
-    num_clones = args.num_clones
-    clone_on_cpu = args.clone_on_cpu
-
-    if args.pipeline_config_path:
-        model_config, train_config, input_config = get_configs_from_pipeline_file(args)
-    else:
-        model_config, train_config, input_config = get_configs_from_multiple_files(args)
-
-    model_fn = functools.partial(model_builder.build, model_config=model_config, is_training=True)
-
-    create_input_dict_fn = functools.partial(input_reader_builder.build, input_config)
-
-    env = json.loads(os.environ.get('TF_CONFIG', '{}'))
-    cluster_data = env.get('cluster', None)
-    cluster = tf.train.ClusterSpec(cluster_data) if cluster_data else None
-    task_data = env.get('task', None) or {'type': 'master', 'index': 0}
-    task_info = type('TaskSpec', (object,), task_data)
-
-    # Parameters for a single worker.
-    ps_tasks = 0
-    worker_replicas = 1
-    worker_job_name = 'lonely_worker'
-    task = 0
-    is_chief = True
-    master = ''
-
-    if cluster_data and 'worker' in cluster_data:
-        # Number of total worker replicas include "worker"s and the "master".
-        worker_replicas = len(cluster_data['worker']) + 1
-    if cluster_data and 'ps' in cluster_data:
-        ps_tasks = len(cluster_data['ps'])
-
-    if worker_replicas > 1 and ps_tasks < 1:
-        raise ValueError('At least 1 ps task is needed for distributed training.')
-
-    if worker_replicas >= 1 and ps_tasks > 0:
-        # Set up distributed training.
-        server = tf.train.Server(tf.train.ClusterSpec(cluster), protocol='grpc',
-                                 job_name=task_info.type,
-                                 task_index=task_info.index)
-        if task_info.type == 'ps':
-            server.join()
-            return
-
-        worker_job_name = '%s/task:%d' % (task_info.type, task_info.index)
-        task = task_info.index
-        is_chief = (task_info.type == 'master')
-        master = server.target
-
-    trainer.train(create_input_dict_fn, model_fn, train_config, master, task, num_clones, worker_replicas,
-                  clone_on_cpu, ps_tasks, worker_job_name, is_chief, train_dir)
-
-
-def get_configs_from_pipeline_file(args):
-    """Reads training configuration from a pipeline_pb2.TrainEvalPipelineConfig.
-
-    Reads training config from file specified by pipeline_config_path flag.
-
-    Returns:
-      model_config: model_pb2.DetectionModel
-      train_config: train_pb2.TrainConfig
-      input_config: input_reader_pb2.InputReader
-    """
-    pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
-    with tf.gfile.GFile(args.pipeline_config_path, 'r') as f:
-        text_format.Merge(f.read(), pipeline_config)
-
-    model_config = pipeline_config.model
-    train_config = pipeline_config.train_config
-    input_config = pipeline_config.train_input_reader
-
-    return model_config, train_config, input_config
-
-
-def get_configs_from_multiple_files(args):
-    """Reads training configuration from multiple config files.
-
-    Reads the training config from the following files:
-      model_config: Read from --model_config_path
-      train_config: Read from --train_config_path
-      input_config: Read from --input_config_path
-
-    Returns:
-      model_config: model_pb2.DetectionModel
-      train_config: train_pb2.TrainConfig
-      input_config: input_reader_pb2.InputReader
-    """
-    train_config = train_pb2.TrainConfig()
-    with tf.gfile.GFile(args.train_config_path, 'r') as f:
-        text_format.Merge(f.read(), train_config)
-
-    model_config = model_pb2.DetectionModel()
-    with tf.gfile.GFile(args.model_config_path, 'r') as f:
-        text_format.Merge(f.read(), model_config)
-
-    input_config = input_reader_pb2.InputReader()
-    with tf.gfile.GFile(args.input_config_path, 'r') as f:
-        text_format.Merge(f.read(), input_config)
-
-    return model_config, train_config, input_config
