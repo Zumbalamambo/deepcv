b02b31b723d168d19f8166329eb3b763c1f2aeca
xhzcyc <sjsanlang@qq.com>
2017-07-06 11:47:10 +0800

    version 0.1

--- app/classifier.py
+++ app/classifier.py
@@ -27,8 +27,8 @@ def run(config, args):
             print('Classification error!')
     elif args.task == 'train':
         pass
-    elif args.task == 'test':
-        test()
+    # elif args.task == 'test':
+    #     test()
     else:
         print('No this task!')
 
@@ -157,43 +157,43 @@ def classify_iamge_remote(config, args):
 
     return top5_result
 
-
-def test():
-    image_size = vgg.vgg_16.default_image_size
-    with tf.device('/gpu:0'):
-        url = 'https://upload.wikimedia.org/wikipedia/commons/d/d9/First_Student_IC_school_bus_202076.jpg'
-        image_string = urllib.urlopen(url).read()
-        image = tf.image.decode_jpeg(image_string, channels=3)
-        preprocess_image_fn = preprocessing_factory.get_preprocessing('vgg_16', is_training=True)
-
-        processed_image = preprocess_image_fn(image, image_size, image_size)
-
-        processed_image = vgg_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
-        processed_images = tf.expand_dims(processed_image, 0)
-
-        # Create the model, use the default arg scope to configure the batch norm parameters.
-        with slim.arg_scope(vgg.vgg_arg_scope()):
-            # 1000 classes instead of 1001.
-            logits, _ = vgg.vgg_16(processed_images, num_classes=1000, is_training=False)
-        probabilities = tf.nn.softmax(logits)
-
-        init_fn = slim.assign_from_checkpoint_fn(
-            os.path.join('cache/weight/vgg', 'vgg_16.ckpt'),
-            slim.get_model_variables('vgg_16'))
-
-        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
-            init_fn(sess)
-            np_image, probabilities = sess.run([image, probabilities])
-            probabilities = probabilities[0, 0:]
-            sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x: x[1])]
-
-        # plt.figure()
-        # plt.imshow(np_image.astype(np.uint8))
-        # plt.axis('off')
-        # plt.show()
-
-        names = imagenet.create_readable_names_for_imagenet_labels()
-        for i in range(5):
-            index = sorted_inds[i]
-            # Shift the index of a class name by one.
-            print('Probability %0.2f%% => [%s]' % (probabilities[index] * 100, names[index + 1]))
+#
+# def test():
+#     image_size = vgg.vgg_16.default_image_size
+#     with tf.device('/gpu:0'):
+#         url = 'https://upload.wikimedia.org/wikipedia/commons/d/d9/First_Student_IC_school_bus_202076.jpg'
+#         image_string = urllib.urlopen(url).read()
+#         image = tf.image.decode_jpeg(image_string, channels=3)
+#         preprocess_image_fn = preprocessing_factory.get_preprocessing('vgg_16', is_training=True)
+#
+#         processed_image = preprocess_image_fn(image, image_size, image_size)
+#
+#         processed_image = vgg_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
+#         processed_images = tf.expand_dims(processed_image, 0)
+#
+#         # Create the model, use the default arg scope to configure the batch norm parameters.
+#         with slim.arg_scope(vgg.vgg_arg_scope()):
+#             # 1000 classes instead of 1001.
+#             logits, _ = vgg.vgg_16(processed_images, num_classes=1000, is_training=False)
+#         probabilities = tf.nn.softmax(logits)
+#
+#         init_fn = slim.assign_from_checkpoint_fn(
+#             os.path.join('cache/weight/vgg', 'vgg_16.ckpt'),
+#             slim.get_model_variables('vgg_16'))
+#
+#         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+#             init_fn(sess)
+#             np_image, probabilities = sess.run([image, probabilities])
+#             probabilities = probabilities[0, 0:]
+#             sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x: x[1])]
+#
+#         # plt.figure()
+#         # plt.imshow(np_image.astype(np.uint8))
+#         # plt.axis('off')
+#         # plt.show()
+#
+#         names = imagenet.create_readable_names_for_imagenet_labels()
+#         for i in range(5):
+#             index = sorted_inds[i]
+#             # Shift the index of a class name by one.
+#             print('Probability %0.2f%% => [%s]' % (probabilities[index] * 100, names[index + 1]))
